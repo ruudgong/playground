c289e63641d5906845494f458c694c7f4ec37908
snwachukwu <snwachukwu@puppet.smart.lab>
2016-09-17 17:03:38 -0700

    Apache bounce update

--- restart-apache.sh
+++ restart-apache.sh
@@ -3,10 +3,9 @@
 # Bounce httpd Web Server When It breaks or goes down
 # -------------------------------------------------------------------------
 # This is just a simple shell script that can enable you restart apache web server when it breaks.
-# To avoid a contastant manual trigger of this script, you wish to set a cron job for constant autamation
-# As as personal choice, I prefer to set  a systemwide crojob instead of individual user cronjobs. This method
-# gives your the flexibilty to  consolidate your cron at a particular location and not have them all over the place
-# incase you need to troubleshoot system issues relating to cronjobs. 
+# To avoid a contastant manual trigger of this script, you may wish to set# a cron job for constant automation
+# As as personal choice, I prefer to set  a system wide crojob instead of # individual user cronjobs. This method
+# gives your the flexibilty to  consolidate your cron at a particular loca# tion and not have them all over the place when you need to troubleshoot # system issues relating to cronjobs. 
 #
 #----------------------------------------------------------------------------------------------------
 ############# This for loop help to identify all the cronjob and the owner on your system ###########
